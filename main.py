c2fee65e2545de67666674f347d9877a96e17ea5
Era Dorta <e.dorta@tudelft.nl>
2024-03-26 16:26:44 +0100

    Reduce number of layers

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import torch
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 3, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(3, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
